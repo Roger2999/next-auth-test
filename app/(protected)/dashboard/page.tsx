c97400cd46d7ddb2ceff96c5a3d60da188b1d45f
Roger <rogergutierrezmartinez900@gmail.com>
2026-05-03 19:07:31 -0400

    Actualizar page.tsx

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default async function Dashboard() {
   return (
     <div className="text-2xl">
       <h2>Welcome to dashboard {session.user.name}</h2>
-      <h3>Your ip address is {session?.session?.ipaddress}</h3>
+      <h3>Your ip address is {session?.session?.ipaddress??"Unknown"}</h3>
     </div>
   );
 }

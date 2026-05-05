cc2ee32fd43bbc2aa2348fefad62cfb0fef5df8c
Roger <rogergutierrezmartinez900@gmail.com>
2026-05-05 18:20:04 -0400

    Actualizar page.tsx

--- app/(public)/(auth)/signin/page.tsx
+++ app/(public)/(auth)/signin/page.tsx
@@ -5,7 +5,7 @@ import { FolderGit2 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
 import { BetterFetchError } from "better-auth/react";
-import { siginGitHUb } from "../services/signinGitHub.service";
+import { signinGitHUb } from "../services/signinGitHub.service";
 import SigninForm from "../components/signin-form";
 
 export default function SignIn() {
@@ -20,7 +20,7 @@ export default function SignIn() {
       <Button
         className="flex w-72 max-w-[80%] gap-2"
         variant={"default"}
-        onClick={() => siginGitHUb(setError, setIsLoading)}
+        onClick={() => signinGitHUb(setError, setIsLoading)}
       >
         {isLoading ? "Loading..." : "Sign in with GitHub"}
         <FolderGit2 />

185334e969697a518d84002233b24a51eb9ba7ec
Roger <rogergutierrezmartinez900@gmail.com>
2026-05-05 18:36:55 -0400

    Actualizar signinGitHub.service.ts

--- app/(public)/(auth)/services/signinGitHub.service.ts
+++ app/(public)/(auth)/services/signinGitHub.service.ts
@@ -1,32 +1,37 @@
 "use client";
 
 import { authClient } from "@/app/lib/auth-client";
+import { BetterFetchError } from "better-auth/react";
 import { Dispatch, SetStateAction } from "react";
 
-export const signInGitHub = (
-  onErrorChange: Dispatch<SetStateAction<string | null>>,
+export const siginGitHUb = async (
+  onErrorChange: Dispatch<
+    SetStateAction<(BetterFetchError & Record<string, unknown>) | undefined>
+  >,
   onLoadingChange: Dispatch<SetStateAction<boolean>>,
 ) => {
-  onLoadingChange(true);
-
-  return authClient.signIn.social(
-    {
-      provider: "github",
-      callbackURL: "/dashboard",
-    },
-    {
-      onRequest: () => {
-        onLoadingChange(true);
+  try {
+    await authClient.signIn.social(
+      {
+        provider: "github",
+        callbackURL: "/dashboard",
       },
-      onError: (ctx) => {
-        console.error("Better Auth error:", ctx.error);
-        onErrorChange(ctx.error.message ?? "Unknown error");
-        onLoadingChange(false);
-
-        setTimeout(() => {
-          onErrorChange(null);
-        }, 6000);
+      {
+        onError: (ctx) => {
+          onErrorChange(ctx.error);
+          setTimeout(() => {
+            onErrorChange(undefined);
+          }, 6000);
+        },
+        onRequest: () => {
+          onLoadingChange(true);
+        },
       },
-    },
-  );
+    );
+  } catch (error) {
+    console.error("GitHub sign-in error:", error);
+    onErrorChange(error as BetterFetchError & Record<string, unknown>);
+  } finally {
+    onLoadingChange(false);
+  }
 };

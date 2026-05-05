a90713fe7efa27a513fa89c52e5d09271120fa39
Roger <rogergutierrezmartinez900@gmail.com>
2026-05-05 18:38:01 -0400

    Actualizar signinGitHub.service.ts

--- app/(public)/(auth)/services/signinGitHub.service.ts
+++ app/(public)/(auth)/services/signinGitHub.service.ts
@@ -4,7 +4,7 @@ import { authClient } from "@/app/lib/auth-client";
 import { BetterFetchError } from "better-auth/react";
 import { Dispatch, SetStateAction } from "react";
 
-export const siginGitHUb = async (
+export const signInGitHUb = async (
   onErrorChange: Dispatch<
     SetStateAction<(BetterFetchError & Record<string, unknown>) | undefined>
   >,

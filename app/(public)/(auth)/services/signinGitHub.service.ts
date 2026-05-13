e41da4fa17361a51bcfc17573600fef7ed59357f
roger <rogergutierrezmartinez900@gmail.com>
2026-05-13 13:27:35 -0400

    Simplificar signinGitHub.service eliminando callbacks no utilizados

--- app/(public)/(auth)/services/signinGitHub.service.ts
+++ app/(public)/(auth)/services/signinGitHub.service.ts
@@ -1,30 +1,30 @@
 "use client";
 
 import { authClient } from "@/app/lib/auth-client";
-import { BetterFetchError } from "better-auth/react";
-import { Dispatch, SetStateAction } from "react";
+// import { BetterFetchError } from "better-auth/react";
+// import { Dispatch, SetStateAction } from "react";
 
 export const signInGitHub = async (
-  onErrorChange: Dispatch<
-    SetStateAction<(BetterFetchError & Record<string, unknown>) | undefined>
-  >,
-  onLoadingChange: Dispatch<SetStateAction<boolean>>,
+  // onErrorChange: Dispatch<
+  //   SetStateAction<(BetterFetchError & Record<string, unknown>) | undefined>
+  // >,
+  // onLoadingChange: Dispatch<SetStateAction<boolean>>,
 ) => {
     await authClient.signIn.social(
       {
         provider: "github",
         callbackURL: "/dashboard",
       },
-      {
-        onError: (ctx) => {
-          onErrorChange(ctx.error);
-          setTimeout(() => {
-            onErrorChange(undefined);
-          }, 6000);
-        },
-        onRequest: () => {
-          onLoadingChange(true);
-        },
-      },
+      // {
+      //   // onError: (ctx) => {
+      //   //   onErrorChange(ctx.error);
+      //   //   setTimeout(() => {
+      //   //     onErrorChange(undefined);
+      //   //   }, 6000);
+      //   // },
+      //   // onRequest: () => {
+      //   //   onLoadingChange(true);
+      //   // },
+      // },
     );
 };

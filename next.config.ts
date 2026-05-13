4dde5e3179b72089ac7180d11facd443064640a3
roger <rogergutierrezmartinez900@gmail.com>
2026-05-13 13:56:13 -0400

    Agregar avatars.githubusercontent.com a remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "via.placeholder.com",
+        hostname: "avatars.githubusercontent.com",
       },
     ],
   },
